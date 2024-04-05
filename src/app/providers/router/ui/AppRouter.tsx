4d902041b36478762a4d7cdb1c6ccbbdef192159
Maryna Shavlak <shavlakmaryna@gmail.com>
2024-04-05 19:26:59 +0300

    add initial structure

---
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -0,0 +1,16 @@
+import React, {Suspense} from 'react';
+import {Route, Routes} from "react-router-dom";
+import {routesConfig} from "shared/config/routeConfig/routeConfig";
+
+export const AppRouter = () => {
+    return (
+        <Suspense fallback={<div>Loading...</div>}>
+            <Routes>
+                {Object.values(routesConfig).map(({path, element})=> (
+                    <Route key ={path} path={path} element={element} />
+                ) )}
+            </Routes>
+        </Suspense>
+    );
+};
+
--- src/test.ts
+++
@@ -1,3 +0,0 @@
-export function hh() {
-    console.log('hhhh')
-}
