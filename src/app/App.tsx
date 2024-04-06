92db5615bf532bd40d2817f7358d119512454c49
Maryna Shavlak <shavlakmaryna@gmail.com>
2024-04-06 13:36:14 +0300

    add stylelint

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -13,6 +13,7 @@ const App = () => {
         <div className={classNames('app', {}, [theme])}>
             <Suspense fallback="">
                 <Navbar />
+
                 <div className="content-page">
                     <Sidebar />
                     <AppRouter />
--- src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -2,7 +2,6 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import React from 'react';
 import { Theme, useTheme } from 'app/providers/ThemeProvider';
 import { Button, ThemeButton } from 'shared/ui/Button/Button';
-import cls from './ThemeSwitcher.module.scss';
 import DarkIcon from '../assets/icons/theme-dark.svg';
 import LightIcon from '../assets/icons/theme-light.svg';
 
@@ -16,7 +15,7 @@ export const ThemeSwitcher = ({ className }: ThemeSwitcherProps) => {
         <Button
             theme={ThemeButton.CLEAR}
             onClick={toggleTheme}
-            className={classNames(cls.ThemeSwitcher, {}, [className])}
+            className={classNames('', {}, [className])}
         >
             {theme === Theme.DARK ? <DarkIcon /> : <LightIcon /> }
         </Button>
