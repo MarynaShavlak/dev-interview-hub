af0679e95e726436834ef41750f5503803a9c12b
Maryna Shavlak <shavlakmaryna@gmail.com>
2024-04-17 17:11:54 +0300

    fix global theme

--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -3,7 +3,6 @@ import React, {
 } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { Portal } from 'shared/ui/Portal/Portal';
-import { useTheme } from 'app/providers/ThemeProvider';
 import cls from './Modal.module.scss';
 
 interface ModalProps {
@@ -24,7 +23,6 @@ export const Modal = (props: ModalProps) => {
     } = props;
     const [isClosing, setIsClosing] = useState(false);
     const timerRef = useRef<ReturnType <typeof setTimeout>>();
-    const { theme } = useTheme();
 
     const closeHandler = useCallback(() => {
         if (onClose) {
@@ -64,7 +62,7 @@ export const Modal = (props: ModalProps) => {
 
     return (
         <Portal>
-            <div className={classNames(cls.Modal, mods, [className, theme])}>
+            <div className={classNames(cls.Modal, mods, [className])}>
                 <div className={cls.overlay} onClick={closeHandler}>
                     <div
                         className={cls.content}
