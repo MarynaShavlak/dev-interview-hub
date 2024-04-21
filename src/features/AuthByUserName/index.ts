d2c322dbbdf250fb7cdccf66ad3e85d2aaa915a0
Maryna Shavlak <shavlakmaryna@gmail.com>
2024-04-21 13:25:49 +0300

    fix path

--- src/features/AuthByUserName/index.ts
+++ src/features/AuthByUserName/index.ts
@@ -1,3 +1,6 @@
-export { loginReducer } from '../AuthByUsername/model/slice/loginSlice';
-export { LoginModal } from '../AuthByUsername/ui/LoginModal/LoginModal';
-export { LoginSchema } from '../AuthByUsername/model/types/loginSchema';
+import { LoginModal } from 'features/AuthByUsername/ui/LoginModal/LoginModal';
+import { loginReducer } from './model/slice/loginSlice';
+
+import { LoginSchema } from './model/types/loginSchema';
+
+export { loginReducer, LoginModal, LoginSchema };
