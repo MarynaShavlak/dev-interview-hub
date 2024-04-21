f4bcc1aa5dc91d6925214688ca36f080a2a7531a
Maryna Shavlak <shavlakmaryna@gmail.com>
2024-04-21 13:36:49 +0300

    delete pipeline

--- src/features/AuthByUserName/index.ts
+++
@@ -1,6 +0,0 @@
-import { LoginModal } from 'features/AuthByUsername/ui/LoginModal/LoginModal';
-import { loginReducer } from './model/slice/loginSlice';
-
-import { LoginSchema } from './model/types/loginSchema';
-
-export { loginReducer, LoginModal, LoginSchema };
