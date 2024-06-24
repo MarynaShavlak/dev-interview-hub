005b6d566bfae8a6f17a8aa4aba1def3ddd7af4f
Maryna Shavlak <shavlakmaryna@gmail.com>
2024-06-24 16:31:01 +0300

    add lint staged

--- src/features/ArticleSortSelector/ui/ArticleSortSelector/ArticleSortSelector.stories.tsx
+++ src/features/ArticleSortSelector/ui/ArticleSortSelector/ArticleSortSelector.stories.tsx
@@ -14,6 +14,4 @@ export default {
 const Template: ComponentStory<typeof ArticleSortSelector> = (args) => <ArticleSortSelector {...args} />;
 
 export const Normal = Template.bind({});
-Normal.args = {
-   
-};
+Normal.args = {};
