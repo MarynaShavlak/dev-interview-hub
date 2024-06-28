5c0485be76af0587b703137cb82b69cab2753592
Maryna Shavlak <shavlakmaryna@gmail.com>
2024-06-28 13:58:53 +0300

    fix remove-feature script

--- scripts/remove-feature/remove-feature.ts
+++ scripts/remove-feature/remove-feature.ts
@@ -53,13 +53,13 @@ function isToggleComponent(node: Node) {
 files.forEach((sourceFile) => {
     sourceFile.forEachDescendant((node) => {
         if (node.isKind(SyntaxKind.CallExpression) && isToggleFunction(node)) {
-            replaceToggleFunction(node);
+            return replaceToggleFunction(node);
         }
         if (
             node.isKind(SyntaxKind.JsxSelfClosingElement) &&
             isToggleComponent(node)
         ) {
-            replaceComponent(node);
+            return replaceComponent(node);
         }
     });
 });
