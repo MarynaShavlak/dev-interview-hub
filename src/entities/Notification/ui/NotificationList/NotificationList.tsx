fc068b5835b845fe11113ec4fde3d9883a0b3932
Maryna Shavlak <shavlakmaryna@gmail.com>
2024-07-03 13:44:37 +0300

    fix redesigned Notification List

--- src/entities/Notification/ui/NotificationList/NotificationList.tsx
+++ src/entities/Notification/ui/NotificationList/NotificationList.tsx
@@ -1,8 +1,10 @@
 import { memo } from 'react';
+import { toggleFeatures } from '@/shared/lib/features';
 import { Each } from '@/shared/lib/components/Each/Each';
 import { classNames } from '@/shared/lib/classNames/classNames';
 import { VStack } from '@/shared/ui/redesigned/Stack';
-import { Skeleton } from '@/shared/ui/deprecated/Skeleton';
+import { Skeleton as SkeletonDeprecated } from '@/shared/ui/deprecated/Skeleton';
+import { Skeleton as SkeletonRedesigned } from '@/shared/ui/redesigned/Skeleton';
 import { useNotifications } from '../../api/notificationApi';
 import cls from './NotificationList.module.scss';
 import { NotificationItem } from '../NotificationItem/NotificationItem';
@@ -15,6 +17,12 @@ export const NotificationList = memo((props: NotificationListProps) => {
     const { className } = props;
     const { data, isLoading } = useNotifications(null, {
         pollingInterval: 10000,
+    });
+
+    const Skeleton = toggleFeatures({
+        name: 'isAppRedesigned',
+        on: () => SkeletonRedesigned,
+        off: () => SkeletonDeprecated,
     });
 
     if (isLoading) {
