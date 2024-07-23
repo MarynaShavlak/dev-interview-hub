2d4748585767e5152fc3490a8493a1e1d433c085
Maryna Shavlak <shavlakmaryna@gmail.com>
2024-07-23 20:44:37 +0300

    add docs for tests

--- src/shared/lib/getFlexClasses/getFlexClasses.ts
+++ src/shared/lib/getFlexClasses/getFlexClasses.ts
@@ -5,7 +5,19 @@ import {
 } from '@/shared/const/flexClasses';
 import cls from '@/shared/styles/flexStyles.module.scss';
 import { FlexAlign, FlexGap, FlexJustify } from '@/shared/types/flexTypes';
-
+/**
+ * The `getFlexClasses` function generates an array of CSS class names based on the provided flexbox configuration options.
+ * It is useful for dynamically applying CSS classes to flex container elements based on alignment, justification, and spacing requirements.
+ *
+ * @param vStack - Optional. A boolean indicating if vertical stacking should be applied. If `true`, the `vStack` class is included in the result.
+ * @param hStack - Optional. A boolean indicating if horizontal stacking should be applied. If `true`, the `hStack` class is included in the result.
+ * @param gap - Optional. A value of type `FlexGap` representing the spacing between flex items. If provided, the corresponding class from `mapGapToClass` is included.
+ * @param justify - Optional. A value of type `FlexJustify` representing the alignment of flex items along the main axis. If provided, the corresponding class from `mapJustifyToClass` is included.
+ * @param align - Optional. A value of type `FlexAlign` representing the alignment of flex items along the cross axis. If provided, the corresponding class from `mapAlignToClass` is included.
+ *
+ * @returns An array of CSS class names based on the provided options. The array includes classes for vertical or horizontal stacking, gap, justification, and alignment as applicable.
+ *
+ * */
 export const getFlexClasses = ({
     vStack,
     hStack,
