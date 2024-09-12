420eada6de42d8a5abcfdf736afbc6d35769394f
Maryna Shavlak <shavlakmaryna@gmail.com>
2024-09-12 15:53:55 +0300

    Navbar storycases

--- src/widgets/Navbar/ui/Navbar.stories.tsx
+++ src/widgets/Navbar/ui/Navbar.stories.tsx
@@ -1,35 +1,43 @@
-export function con() {
-    console.log('stories');
-}
-// import React from 'react';
-// import { ComponentStory, ComponentMeta } from '@storybook/react';
-// import { Theme } from '@/shared/const/theme';
-//
-// import { ThemeDecorator } from '@/shared/config/storybook/ThemeDecorator/ThemeDecorator';
-// import { StoreDecorator } from '@/shared/config/storybook/StoreDecorator/StoreDecorator';
-// import { Navbar } from './Navbar';
-//
-// export default {
-//     title: 'widgets/Navbar',
-//     component: Navbar,
-//     argTypes: {
-//         backgroundColor: { control: 'color' },
-//     },
-// } as ComponentMeta<typeof Navbar>;
-//
-// const Template: ComponentStory<typeof Navbar> = (args) => <Navbar {...args} />;
-//
-// export const Light = Template.bind({});
-// Light.args = {};
-// Light.decorators = [StoreDecorator({})];
-// export const Dark = Template.bind({});
-// Dark.args = {};
-// Dark.decorators = [ThemeDecorator(Theme.DARK), StoreDecorator({})];
-//
-// export const AuthNavbar = Template.bind({});
-// AuthNavbar.args = {};
-// AuthNavbar.decorators = [
-//     StoreDecorator({
-//         user: { authData: {} },
-//     }),
-// ];
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { StoreDecorator } from '@/shared/config/storybook/StoreDecorator/StoreDecorator';
+import { Navbar } from './Navbar';
+import { NewDesignDecorator } from '@/shared/config/storybook/NewDesignDecorator/NewDesignDecorator';
+import { AlignRightDecorator } from '@/shared/config/storybook/AlignDecorator/AlignCenterDecorator';
+
+export default {
+    title: 'widgets/Navbar',
+    component: Navbar,
+    argTypes: {
+        backgroundColor: { control: 'color' },
+    },
+} as ComponentMeta<typeof Navbar>;
+
+const Template: ComponentStory<typeof Navbar> = (args) => <Navbar {...args} />;
+
+export const Normal = Template.bind({});
+Normal.args = {};
+Normal.decorators = [StoreDecorator({})];
+
+export const NormalRedesigned = Template.bind({});
+NormalRedesigned.args = {};
+NormalRedesigned.decorators = [StoreDecorator({}), NewDesignDecorator];
+
+export const AuthNavbar = Template.bind({});
+AuthNavbar.args = {};
+AuthNavbar.decorators = [
+    StoreDecorator({
+        user: { authData: {} },
+    }),
+];
+
+export const AuthNavbarRedesigned = Template.bind({});
+AuthNavbarRedesigned.args = {};
+AuthNavbarRedesigned.decorators = [
+    StoreDecorator({
+        user: { authData: {} },
+    }),
+    AlignRightDecorator,
+    NewDesignDecorator,
+];
