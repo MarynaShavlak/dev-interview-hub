db97fedb903533115fd4bb439da2e9d1c5e416f8
Maryna Shavlak <shavlakmaryna@gmail.com>
2024-11-27 13:55:58 +0200

    add in forestore updated articles data

--- src/entities/Article/index.ts
+++ src/entities/Article/index.ts
@@ -1,3 +1,5 @@
+export { ArticleSection } from './model/consts/articleConsts';
+
 export type { Article } from './model/types/article';
 export type { ArticleDetailsSchema } from './model/types/articleDetailsSchema';
 export {
