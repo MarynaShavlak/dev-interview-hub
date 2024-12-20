63fa912163892e95075f05e2b12d7f85eac42db4
Maryna Shavlak <shavlakmaryna@gmail.com>
2024-12-20 17:00:28 +0200

    add language selector code

--- src/pages/ArticleCreatePage/ui/CodeBlockEditor/CodeBlockEditor.tsx
+++ src/pages/ArticleCreatePage/ui/CodeBlockEditor/CodeBlockEditor.tsx
@@ -1,21 +1,24 @@
 import React, { useRef, useState } from 'react';
 import { Editor, OnMount } from '@monaco-editor/react';
 import { useTranslation } from 'react-i18next';
-import { editor } from 'monaco-editor';
+import * as monaco from 'monaco-editor';
 import { Input } from '@/shared/ui/redesigned/Input';
 import cls from '../ArticleCreatePage/ArticleCreatePage.module.scss';
 import { VStack } from '@/shared/ui/common/Stack';
 import { useBlockTitle } from '../../lib/hooks/useBlockTitle/useBlockTitle';
 import { useTheme } from '@/shared/lib/hooks/useTheme/useTheme';
 import { Theme } from '@/shared/const/theme';
-import { CODE_SNIPPETS } from '../../model/consts/codeLanguages';
+import {
+    CODE_SNIPPETS,
+    LANGUAGE_VERSIONS,
+} from '../../model/consts/codeLanguages';
 import { Language } from '../../model/types/language';
-
-type MonacoDiffEditor = editor.IStandaloneDiffEditor;
+import { ListBox } from '@/shared/ui/redesigned/Popups';
 
 export const CodeBlockEditor: React.FC = () => {
-    const editorRef = useRef();
+    const editorRef = useRef<monaco.editor.IStandaloneCodeEditor | null>(null);
     const [value, setValue] = useState<string>('');
+    console.log('value', value);
     const [language, setLanguage] = useState<Language>('javascript');
     const { t } = useTranslation('articleDetails');
     const { theme } = useTheme();
@@ -24,13 +27,28 @@ export const CodeBlockEditor: React.FC = () => {
     const { title, handleTitleChange, validConfig } = useBlockTitle();
 
     const onMount: OnMount = (editor, monaco) => {
-        // @ts-ignore
         editorRef.current = editor;
         editor.focus();
     };
 
+    const onSelectLanguage = (language: Language) => {
+        setLanguage(language);
+        setValue(CODE_SNIPPETS[language]);
+    };
+
+    const languageOptions = Object.keys(LANGUAGE_VERSIONS).map((key) => ({
+        value: key,
+        label: key,
+    }));
     return (
         <VStack gap="16">
+            <ListBox
+                value={language}
+                defaultValue="javascript"
+                items={languageOptions}
+                onChange={onSelectLanguage}
+                direction="bottom right"
+            />
             <Editor
                 options={{
                     minimap: {
@@ -41,11 +59,11 @@ export const CodeBlockEditor: React.FC = () => {
                 height="75vh"
                 theme={codeTheme}
                 language={language}
-                defaultLanguage="javascript"
+                defaultLanguage={CODE_SNIPPETS.javascript}
                 defaultValue={CODE_SNIPPETS[language]}
                 onMount={onMount}
                 value={value}
-                // onChange={(newValue) => setValue(newValue || '')}
+                onChange={(newValue) => setValue(newValue || '')}
             />
             <Input
                 value={title}
