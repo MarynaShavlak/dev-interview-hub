ca81b4f59b792dab072ea5e7a94eeaf2b80de974
Maryna Shavlak <shavlakmaryna@gmail.com>
2025-02-13 19:46:38 +0200

    TableRow

--- src/features/Table/ui/TableRow/TableRow.tsx
+++ src/features/Table/ui/TableRow/TableRow.tsx
@@ -4,6 +4,7 @@ import cls from './TableRow.module.scss';
 import { classNames } from '@/shared/lib/classes/classNames/classNames';
 import { getFlexClasses } from '@/shared/lib/classes/getFlexClasses/getFlexClasses';
 import { Each } from '@/shared/lib/components/Each/Each';
+import { toggleFeatures } from '@/shared/lib/features';
 
 interface TableRowProps<T> {
     row: Row<T>;
@@ -14,13 +15,19 @@ export const TableRow = <T,>({ row }: TableRowProps<T>) => {
         vStack: true,
         justify: 'center',
     });
+
+    const tdClass = toggleFeatures({
+        name: 'isAppRedesigned',
+        on: () => cls.tdRedesigned,
+        off: () => cls.tdDeprecated,
+    });
     return (
         <Box className={cls.tr} key={row.id}>
             <Each
                 of={row.getVisibleCells()}
                 render={(cell) => (
                     <Box
-                        className={classNames(cls.td, {}, [
+                        className={classNames(tdClass, {}, [
                             ...additionalCellClasses,
                         ])}
                         key={cell.id}
