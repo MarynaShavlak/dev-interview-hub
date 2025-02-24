434b4b81ea69ae95544aae0cd495cfdb645d110e
Maryna Shavlak <shavlakmaryna@gmail.com>
2025-02-24 21:02:28 +0200

    fix notification api

--- src/entities/Notification/api/notificationApi.ts
+++ src/entities/Notification/api/notificationApi.ts
@@ -1,69 +1,9 @@
-import { onSnapshot, query } from 'firebase/firestore';
+import { onSnapshot } from 'firebase/firestore';
 import { firestoreApi } from '@/shared/api/rtkApi';
 import { Notification } from '../model/types/notification';
-import { fetchCollection } from '@/shared/lib/firestore/fetchCollection/fetchCollection';
-import { dataPoint } from '@/shared/lib/firestore/firestore';
 
-// const notificationApi = firestoreApi
-//     .enhanceEndpoints({ addTagTypes: ['Notifications'] })
-//     .injectEndpoints({
-//         endpoints: (build) => ({
-//             getNotifications: build.query<Notification[], void>({
-//                 providesTags: ['Notifications'],
-//                 keepUnusedDataFor: 3600,
-//                 async queryFn() {
-//                     try {
-//                         const notifications =
-//                             await fetchCollection<Notification>(
-//                                 'notifications',
-//                             );
-//                         return { data: notifications };
-//                     } catch (error) {
-//                         console.error('Error fetching notifications:', error);
-//                         return { error };
-//                     }
-//                 },
-//                 async onCacheEntryAdded(
-//                     _,
-//                     { updateCachedData, cacheDataLoaded, cacheEntryRemoved },
-//                 ) {
-//                     await cacheDataLoaded;
-//                     let unsubscribe;
-//                     try {
-//                         const collectionRef =
-//                             dataPoint<Notification>('notifications');
-//                         const queryRef = query(collectionRef);
-//                         unsubscribe = onSnapshot(queryRef, (snapshot) => {
-//                             updateCachedData((draft) => {
-//                                 const result = snapshot?.docs?.map((doc) =>
-//                                     doc.data(),
-//                                 ) as Notification[];
-//                             });
-//                         });
-//                     } catch (error) {
-//                         console.error('Error in notifications!', error);
-//                     }
-//
-//                     await cacheEntryRemoved;
-//                     if (unsubscribe) {
-//                         unsubscribe();
-//                     }
-//                 },
-//             }),
-//         }),
-//     });
-//
-// export const useNotifications = notificationApi.useGetNotificationsQuery;
-
-// const notificationApi = rtkApi.injectEndpoints({
-//     endpoints: (build) => ({
-//         getNotifications: build.query<Notification[], null>({
-//             query: () => ({
-//                 url: '/notifications',
-//             }),
-//         }),
-//     }),
-// });
+import { fetchQueryResults } from '@/shared/lib/firestore/fetchQueryResults/fetchQueryResults';
+import { createUserNotificationQuery } from '../lib/utilities/createUserNotificationsQuery/createUserNotificationsQuery';
 
 const notificationApi = firestoreApi
     .enhanceEndpoints({ addTagTypes: ['Notifications'] })
@@ -75,10 +15,14 @@ const notificationApi = firestoreApi
 
                 async queryFn() {
                     try {
+                        const notificationsQuery =
+                            createUserNotificationQuery();
+
                         const notifications =
-                            await fetchCollection<Notification>(
-                                'notifications',
+                            await fetchQueryResults<Notification>(
+                                notificationsQuery,
                             );
+
                         return { data: notifications };
                     } catch (error) {
                         console.error('Error fetching notifications:', error);
@@ -94,21 +38,21 @@ const notificationApi = firestoreApi
                     let unsubscribe;
 
                     try {
-                        const collectionRef =
-                            dataPoint<Notification>('notifications');
-                        const queryRef = query(collectionRef);
+                        const notificationsQuery =
+                            createUserNotificationQuery();
 
-                        unsubscribe = onSnapshot(queryRef, (snapshot) => {
-                            updateCachedData((draft) => {
-                                // Convert snapshot to array of notifications
-                                const notifications = snapshot.docs.map((doc) =>
-                                    doc.data(),
-                                ) as Notification[];
+                        unsubscribe = onSnapshot(
+                            notificationsQuery,
+                            (snapshot) => {
+                                updateCachedData((draft) => {
+                                    const notifications = snapshot?.docs?.map(
+                                        (doc) => doc.data(),
+                                    ) as Notification[];
 
-                                // Replace entire draft with new notifications
-                                return notifications;
-                            });
-                        });
+                                    return notifications;
+                                });
+                            },
+                        );
                     } catch (error) {
                         console.error(
                             'Error in notifications subscription:',
@@ -121,6 +65,53 @@ const notificationApi = firestoreApi
                         unsubscribe();
                     }
                 },
+
+                // async queryFn() {
+                //     try {
+                //         const notifications =
+                //             await fetchCollection<Notification>(
+                //                 'notifications',
+                //             );
+                //         return { data: notifications };
+                //     } catch (error) {
+                //         console.error('Error fetching notifications:', error);
+                //         return { error };
+                //     }
+                // },
+                //
+                // async onCacheEntryAdded(
+                //     _,
+                //     { updateCachedData, cacheDataLoaded, cacheEntryRemoved },
+                // ) {
+                //     await cacheDataLoaded;
+                //     let unsubscribe;
+                //
+                //     try {
+                //         const collectionRef =
+                //             dataPoint<Notification>('notifications');
+                //         const queryRef = query(collectionRef);
+                //
+                //         unsubscribe = onSnapshot(queryRef, (snapshot) => {
+                //             updateCachedData((draft) => {
+                //                 const notifications = snapshot.docs.map((doc) =>
+                //                     doc.data(),
+                //                 ) as Notification[];
+                //
+                //                 return notifications;
+                //             });
+                //         });
+                //     } catch (error) {
+                //         console.error(
+                //             'Error in notifications subscription:',
+                //             error,
+                //         );
+                //     }
+                //
+                //     await cacheEntryRemoved;
+                //     if (unsubscribe) {
+                //         unsubscribe();
+                //     }
+                // },
             }),
         }),
     });
---
+++ src/entities/Notification/lib/utilities/createUserNotificationsQuery/createUserNotificationsQuery.ts
@@ -0,0 +1,14 @@
+import { query, Query, where } from 'firebase/firestore';
+import { dataPoint } from '@/shared/lib/firestore/firestore';
+import { auth } from '../../../../../../json-server/firebase';
+import { Notification } from '../../../model/types/notification';
+
+export const createUserNotificationQuery = (): Query<Notification> => {
+    const user = auth.currentUser;
+    const notificationsCollection = dataPoint<Notification>('notifications');
+    if (!user) {
+        return query(notificationsCollection, where('authorId', '!=', ''));
+    }
+
+    return query(notificationsCollection, where('authorId', '!=', user.uid));
+};
