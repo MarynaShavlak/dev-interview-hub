028542e545693debc39d3069711009190d19025f
Maryna Shavlak <shavlakmaryna@gmail.com>
2025-02-27 20:17:48 +0200

    fix card

--- src/shared/ui/deprecated/Popups/ui/ListBox/ListBox.tsx
+++ src/shared/ui/deprecated/Popups/ui/ListBox/ListBox.tsx
@@ -62,7 +62,7 @@ export function ListBox<T extends string>(props: ListBoxProps<T>) {
     );
     return (
         <HStack gap="4" max={max}>
-            {label && <span>{`${label}>`}</span>}
+            {label && <span className={cls.label}>{`${label}>`}</span>}
             <HListBox
                 disabled={readonly}
                 as="div"
