71890a20676661c8a742d08a909bc120f16a91fc
Maryna Shavlak <shavlakmaryna@gmail.com>
2025-03-02 13:43:06 +0200

    fix code

--- src/entities/User/lib/utilities/updateUserInFirestore/updateUserInFirestore.ts
+++ src/entities/User/lib/utilities/updateUserInFirestore/updateUserInFirestore.ts
@@ -1,23 +1,25 @@
 import { getDoc, updateDoc } from 'firebase/firestore';
 import { User } from '../../../model/types/user';
 import { getUserDocRefById } from '../getUserDocRefById/getUserDocRefById';
+import { ERROR_USER_MESSAGES } from '../../../model/consts/errorUserMessages';
 
 export const updateUserInFirestore = async (
     userId: string,
     updates: Partial<User>,
 ) => {
     const userDocRef = await getUserDocRefById(userId);
-
-    if (userDocRef) {
-        await updateDoc(userDocRef, updates);
-        const updatedDoc = await getDoc(userDocRef);
-        const updatedData = updatedDoc.data();
-
-        if (updatedData) {
-            return {
-                data: updatedData as User,
-            };
-        }
+    if (!userDocRef) {
+        throw new Error(ERROR_USER_MESSAGES.USER_NOT_FOUND(userId));
     }
-    return null;
+
+    await updateDoc(userDocRef, updates);
+    const updatedDoc = await getDoc(userDocRef);
+    const updatedData = updatedDoc.data();
+    if (!updatedData) {
+        throw new Error(
+            ERROR_USER_MESSAGES.UPDATED_DATA_RETRIEVAL_ERROR(userId),
+        );
+    }
+
+    return updatedData as User;
 };
--- src/entities/User/model/consts/errorUserMessages.ts
+++ src/entities/User/model/consts/errorUserMessages.ts
@@ -4,10 +4,12 @@ export const ERROR_USER_MESSAGES = {
     USER_SNAPSHOT_FAIL: (userId: string) =>
         `Error in ser with id "${userId}" snapshot.`,
 
-    USER_NOT_FOUND: 'User not found.',
+    USER_NOT_FOUND: (userId: string) => `User with id "${userId}" not found.`,
     DELETE_USER_ID_FAIL: (userId: string) =>
         `Failed to delete user with ID "${userId}".`,
     UPDATE_USER_ERROR: (userId: string) =>
         `Error updating user with id "${userId}".`,
-    GENERIC_ERROR: 'Something went wrong with users.',
+
+    UPDATED_DATA_RETRIEVAL_ERROR: (userId: string) =>
+        `Failed to retrieve updated data for user ${userId}`,
 };
