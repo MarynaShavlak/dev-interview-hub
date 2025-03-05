5a9c9032f2e6f480701b7d5ea98f058e9f0d655e
Maryna Shavlak <shavlakmaryna@gmail.com>
2025-03-05 17:28:26 +0200

    finish all charts storybook

---
+++ src/shared/ui/common/Charts/ui/TreemapChart/TreemapChart.stories.tsx
@@ -0,0 +1,79 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { TreemapChart } from './TreemapChart';
+import { NewDesignDecorator } from '@/shared/config/storybook/NewDesignDecorator/NewDesignDecorator';
+import { ThemeDecorator } from '@/shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { Theme } from '@/shared/const/theme';
+
+export default {
+    title: 'shared/common/Charts/TreemapChart',
+    component: TreemapChart,
+    argTypes: {
+        data: { control: 'array' },
+        title: { control: 'text' },
+        width: { control: 'text' },
+        height: { control: 'text' },
+    },
+} as ComponentMeta<typeof TreemapChart>;
+
+const Template: ComponentStory<typeof TreemapChart> = (args) => {
+    return (
+        <div style={{ width: '500px', height: '500px', margin: 'auto' }}>
+            <TreemapChart {...args} />
+        </div>
+    );
+};
+
+const defaultArgs = {
+    data: [
+        { x: 'Category A', y: 30 },
+        { x: 'Category B', y: 40 },
+        { x: 'Category C', y: 20 },
+        { x: 'Category D', y: 10 },
+    ],
+    title: 'Treemap Chart Example',
+    width: '400',
+    height: '500',
+};
+
+export const Default = Template.bind({});
+Default.args = defaultArgs;
+
+export const DarkTheme = Template.bind({});
+DarkTheme.args = defaultArgs;
+DarkTheme.decorators = [ThemeDecorator(Theme.DARK)];
+
+export const OrangeTheme = Template.bind({});
+OrangeTheme.args = defaultArgs;
+OrangeTheme.decorators = [ThemeDecorator(Theme.ORANGE)];
+
+export const TreemapChartWithNoData = Template.bind({});
+TreemapChartWithNoData.args = {
+    data: [],
+    title: 'No Data Available',
+    width: '400',
+    height: '500',
+};
+
+export const RedesignedDefault = Template.bind({});
+RedesignedDefault.args = defaultArgs;
+RedesignedDefault.decorators = [NewDesignDecorator];
+
+export const RedesignedDark = Template.bind({});
+RedesignedDark.args = defaultArgs;
+RedesignedDark.decorators = [NewDesignDecorator, ThemeDecorator(Theme.DARK)];
+
+export const RedesignedOrange = Template.bind({});
+RedesignedOrange.args = defaultArgs;
+RedesignedOrange.decorators = [
+    NewDesignDecorator,
+    ThemeDecorator(Theme.ORANGE),
+];
+
+export const WithCustomSize = Template.bind({});
+WithCustomSize.args = {
+    ...defaultArgs,
+    width: '200px',
+    height: '200px',
+};
+WithCustomSize.decorators = [NewDesignDecorator];
