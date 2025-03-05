8f7d1f72fe23c035bf7351a4493cab6b51136dda
Maryna Shavlak <shavlakmaryna@gmail.com>
2025-03-05 14:49:33 +0200

    cancel modal code

--- src/shared/config/storybook/ThemeDecorator/ThemeDecorator.tsx
+++ src/shared/config/storybook/ThemeDecorator/ThemeDecorator.tsx
@@ -1,10 +1,8 @@
 import { Story } from '@storybook/react';
 // eslint-disable-next-line ms-production-project-plugin/layer-imports
-import { I18nextProvider } from 'react-i18next';
 // eslint-disable-next-line ms-production-project-plugin/layer-imports
 import { ThemeProvider } from '@/app/providers/ThemeProvider';
 import { Theme } from '@/shared/const/theme';
-import i18nForTests from '../../i18n/i18nForTests';
 
 /**
  * The `ThemeDecorator` is a Storybook decorator that wraps components in a `ThemeProvider` to allow them to be rendered with a specified theme.
@@ -19,19 +17,17 @@ import i18nForTests from '../../i18n/i18nForTests';
 
 export const ThemeDecorator = (theme: Theme) => (StoryComponent: Story) => (
     <ThemeProvider initialTheme={theme}>
-        <I18nextProvider i18n={i18nForTests}>
-            <div
-                className={`app ${theme}`}
-                style={{
-                    padding: '20px',
-                    position: 'absolute',
-                    top: 0,
-                    left: 0,
-                    width: '100%',
-                }}
-            >
-                <StoryComponent />
-            </div>
-        </I18nextProvider>
+        <div
+            className={`app ${theme}`}
+            style={{
+                padding: '20px',
+                position: 'absolute',
+                top: 0,
+                left: 0,
+                width: '100%',
+            }}
+        >
+            <StoryComponent />
+        </div>
     </ThemeProvider>
 );
--- src/shared/ui/common/ConfirmCancelModal/ui/ConfirmCancelModal/ConfirmCancelModal.stories.tsx
+++ src/shared/ui/common/ConfirmCancelModal/ui/ConfirmCancelModal/ConfirmCancelModal.stories.tsx
@@ -4,6 +4,8 @@ import { action } from '@storybook/addon-actions';
 import { ConfirmCancelModal } from './ConfirmCancelModal';
 import { ThemeDecorator } from '@/shared/config/storybook/ThemeDecorator/ThemeDecorator';
 import { Theme } from '@/shared/const/theme';
+import { withI18nDecorator } from '@/shared/config/storybook/withI18nDecorator/withI18nDecorator';
+import { NewDesignDecorator } from '@/shared/config/storybook/NewDesignDecorator/NewDesignDecorator';
 
 export default {
     title: 'shared/common/ConfirmCancelModal',
@@ -16,29 +18,29 @@ export default {
         confirmBtnText: { control: 'text' },
         isOpen: { control: 'boolean' },
     },
+    decorators: [withI18nDecorator],
 } as ComponentMeta<typeof ConfirmCancelModal>;
 
 const Template: ComponentStory<typeof ConfirmCancelModal> = (args) => (
     <ConfirmCancelModal {...args} />
 );
+const arg = {
+    isOpen: true,
+    onCancel: action('Cancelled'),
+    onConfirm: action('Confirmed'),
+    text: 'редагування цієї статті',
+    cancelBtnText: 'Продовжити редагування',
+    confirmBtnText: 'Відмінити зміни',
+};
 
 export const Default = Template.bind({});
-Default.args = {
-    isOpen: true,
-    onCancel: action('Cancelled'),
-    onConfirm: action('Confirmed'),
-    text: 'редагування цієї статті',
-    cancelBtnText: 'Продовжити редагування',
-    confirmBtnText: 'Відмінити зміни',
-};
+Default.args = arg;
 
 export const DarkTheme = Template.bind({});
-DarkTheme.args = {
-    isOpen: true,
-    onCancel: action('Cancelled'),
-    onConfirm: action('Confirmed'),
-    text: 'редагування цієї статті',
-    cancelBtnText: 'Продовжити редагування',
-    confirmBtnText: 'Відмінити зміни',
-};
+DarkTheme.args = arg;
 DarkTheme.decorators = [ThemeDecorator(Theme.DARK)];
+
+export const DefaultRedesigned = Template.bind({});
+DefaultRedesigned.args = arg;
+
+DefaultRedesigned.decorators = [NewDesignDecorator];
