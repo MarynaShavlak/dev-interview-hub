745a07a20d3839a6d79900dea9e3532e92d144b4
Maryna Shavlak <shavlakmaryna@gmail.com>
2025-03-05 17:26:33 +0200

    stacked columns

---
+++ src/shared/ui/common/Charts/ui/StackedColumnsChart/StackedColumnsChart.stories.tsx
@@ -0,0 +1,90 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { StackedColumnsChart } from './StackedColumnsChart';
+import { NewDesignDecorator } from '@/shared/config/storybook/NewDesignDecorator/NewDesignDecorator';
+import { ThemeDecorator } from '@/shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { Theme } from '@/shared/const/theme';
+
+export default {
+    title: 'shared/common/Charts/StackedColumnsChart',
+    component: StackedColumnsChart,
+    argTypes: {
+        data: { control: 'array' },
+        labels: { control: 'array' },
+        title: { control: 'text' },
+        legendPosition: {
+            control: 'radio',
+        },
+        xAxisTitle: { control: 'text' },
+        yAxisTitle: { control: 'text' },
+    },
+} as ComponentMeta<typeof StackedColumnsChart>;
+
+const Template: ComponentStory<typeof StackedColumnsChart> = (args) => {
+    return (
+        <div style={{ width: '1000px', height: '500px', margin: 'auto' }}>
+            <StackedColumnsChart {...args} />
+        </div>
+    );
+};
+
+const defaultArgs = {
+    data: [
+        { name: 'Category A', data: [30, 40, 35] },
+        { name: 'Category B', data: [20, 30, 25] },
+        { name: 'Category C', data: [10, 15, 20] },
+    ],
+    labels: ['January', 'February', 'March'],
+    title: 'Stacked Columns Chart Example',
+    xAxisTitle: 'Months',
+    yAxisTitle: 'Values',
+    width: '400',
+    height: '500',
+    legendPosition: 'right' as const,
+};
+
+export const Default = Template.bind({});
+Default.args = defaultArgs;
+
+export const DarkTheme = Template.bind({});
+DarkTheme.args = defaultArgs;
+DarkTheme.decorators = [ThemeDecorator(Theme.DARK)];
+
+export const OrangeTheme = Template.bind({});
+OrangeTheme.args = defaultArgs;
+OrangeTheme.decorators = [ThemeDecorator(Theme.ORANGE)];
+
+export const StackedColumnsChartWithNoData = Template.bind({});
+StackedColumnsChartWithNoData.args = {
+    data: [],
+    labels: [],
+    title: 'No Data Available',
+    xAxisTitle: 'Months',
+    yAxisTitle: 'Values',
+    width: '400',
+    height: '500',
+};
+
+export const RedesignedDefault = Template.bind({});
+RedesignedDefault.args = defaultArgs;
+RedesignedDefault.decorators = [NewDesignDecorator];
+
+export const RedesignedDark = Template.bind({});
+RedesignedDark.args = defaultArgs;
+RedesignedDark.decorators = [NewDesignDecorator, ThemeDecorator(Theme.DARK)];
+
+export const RedesignedOrange = Template.bind({});
+RedesignedOrange.args = defaultArgs;
+RedesignedOrange.decorators = [
+    NewDesignDecorator,
+    ThemeDecorator(Theme.ORANGE),
+];
+
+export const WithCustomSize = Template.bind({});
+WithCustomSize.args = {
+    ...defaultArgs,
+    width: '800px',
+    height: '280px',
+    legendPosition: 'top' as const,
+};
+WithCustomSize.decorators = [NewDesignDecorator];
