37d6c52d2fda4e5a3e819261ec86f53eb3845725
Maryna Shavlak <shavlakmaryna@gmail.com>
2025-03-05 11:26:35 +0200

    fix stories

---
+++ src/shared/ui/redesigned/List/List.stories.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { List, ListProps } from './List';
+import { NewDesignDecorator } from '@/shared/config/storybook/NewDesignDecorator/NewDesignDecorator';
+
+export default {
+    title: 'shared/redesigned/List',
+    component: List,
+    argTypes: {
+        variant: { control: 'radio', options: ['primary', 'accent'] },
+        type: { control: 'radio', options: ['ordered', 'unordered'] },
+        align: { control: 'radio', options: ['left', 'center', 'right'] },
+        bold: { control: 'boolean' },
+    },
+    parameters: {
+        docs: {
+            source: {
+                type: 'code',
+            },
+        },
+    },
+    decorators: [NewDesignDecorator],
+} as ComponentMeta<typeof List>;
+
+const Template: ComponentStory<typeof List> = (args: ListProps) => (
+    <List {...args} />
+);
+
+export const Default = Template.bind({});
+Default.args = {
+    items: ['Item 1', 'Item 2', 'Item 3'],
+};
+
+export const OrderedList = Template.bind({});
+OrderedList.args = {
+    items: ['First', 'Second', 'Third'],
+    type: 'ordered',
+};
+
+export const BoldList = Template.bind({});
+BoldList.args = {
+    items: ['Bold Item 1', 'Bold Item 2'],
+    bold: true,
+};
+
+export const AccentCentered = Template.bind({});
+AccentCentered.args = {
+    items: ['Item 1', 'Item 2'],
+    variant: 'accent',
+    align: 'center',
+};
--- src/shared/ui/redesigned/List/List.tsx
+++ src/shared/ui/redesigned/List/List.tsx
@@ -5,33 +5,24 @@ import { classNames } from '@/shared/lib/classes/classNames/classNames';
 import cls from './List.module.scss';
 import { getFlexClasses } from '@/shared/lib/classes/getFlexClasses/getFlexClasses';
 
-export type ListVariant = 'primary' | 'accent' | 'error';
-export type ListSize = 's' | 'm' | 'l';
+export type ListVariant = 'primary' | 'accent';
 export type ListType = 'ordered' | 'unordered';
 
-interface ListProps extends TestProps {
+export interface ListProps extends TestProps {
     className?: string;
     items: string[];
-
     variant?: ListVariant;
-    size?: ListSize;
     type?: ListType;
     align?: 'left' | 'center' | 'right';
     bold?: boolean;
 }
-
-const mapSizeToClass: Record<ListSize, string> = {
-    s: cls.size_s,
-    m: cls.size_m,
-    l: cls.size_l,
-};
 
 export const List = memo((props: ListProps) => {
     const {
         className,
         items,
         variant = 'primary',
-        size = 'm',
+
         type = 'unordered',
         align = 'left',
         bold = false,
@@ -40,10 +31,8 @@ export const List = memo((props: ListProps) => {
 
     const ListTag = type === 'ordered' ? 'ol' : 'ul';
     const itemClass = type === 'ordered' ? cls.ordered : cls.unordered;
-    const sizeClass = mapSizeToClass[size];
-    const mainClasses = [className, cls[variant], cls[align], sizeClass].filter(
-        Boolean,
-    );
+
+    const mainClasses = [className, cls[variant], cls[align]].filter(Boolean);
 
     const additionalClasses = getFlexClasses({
         vStack: true,
@@ -72,44 +61,3 @@ export const List = memo((props: ListProps) => {
         </ListTag>
     );
 });
-// const renderListItems = (listItems: (string | string[])[]) => {
-//     return listItems.map((item, index) => {
-//         if (Array.isArray(item)) {
-//             // Render a nested list if item is an array
-//             return (
-//                 <li
-//                     key={index}
-//                     className={itemClass}
-//                     data-testid={`${dataTestId}.NestedItem.${index}`}
-//                 >
-//                     <ListTag>
-//                         {renderListItems(item)} {/* Recursive rendering */}
-//                     </ListTag>
-//                 </li>
-//             );
-//         }
-//         // Sanitize and render a simple list item
-//         const sanitizedItem = DOMPurify.sanitize(item);
-//         return (
-//             <li
-//                 key={index}
-//                 className={itemClass}
-//                 data-testid={`${dataTestId}.Item.${index}`}
-//                 dangerouslySetInnerHTML={{ __html: sanitizedItem }}
-//             />
-//         );
-//     });
-// };
-
-// return (
-//     <ListTag
-//         className={classNames(cls.List, { [cls.bold]: bold }, [
-//             ...mainClasses,
-//             ...additionalClasses,
-//         ])}
-//         data-testid={dataTestId}
-//     >
-//         {renderListItems(items)}
-//     </ListTag>
-// );
-// });
