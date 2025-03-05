9ec5c452262eda68b1555213b54855de9ead7e75
Maryna Shavlak <shavlakmaryna@gmail.com>
2025-03-05 15:48:25 +0200

    fix avatar

--- src/shared/ui/redesigned/Avatar/Avatar.stories.tsx
+++ src/shared/ui/redesigned/Avatar/Avatar.stories.tsx
@@ -16,29 +16,38 @@ const Template: ComponentStory<typeof Avatar> = (args) => <Avatar {...args} />;
 
 export const Default = Template.bind({});
 Default.args = {
-    src: 'https://via.placeholder.com/100',
+    src: 'https://img.freepik.com/premium-vector/vector-flat-illustration-grayscale-avatar-user-profile-person-icon-profile-picture-business-profile-woman-suitable-social-media-profiles-icons-screensavers-as-templatex9_719432-1339.jpg',
     alt: 'User Avatar',
     size: 100,
 };
 
 export const WithUserName = Template.bind({});
 WithUserName.args = {
-    src: 'https://via.placeholder.com/150',
+    src: 'https://img.freepik.com/premium-vector/vector-flat-illustration-grayscale-avatar-user-profile-person-icon-profile-picture-business-profile-woman-suitable-social-media-profiles-icons-screensavers-as-templatex9_719432-1339.jpg',
     alt: 'User Avatar',
     size: 30,
     userName: 'Name of user',
 };
 
+export const WithTextLength = Template.bind({});
+WithTextLength.args = {
+    src: 'https://img.freepik.com/premium-vector/vector-flat-illustration-grayscale-avatar-user-profile-person-icon-profile-picture-business-profile-woman-suitable-social-media-profiles-icons-screensavers-as-templatex9_719432-1339.jpg',
+    alt: 'User Avatar',
+    size: 30,
+    userName: 'TestUsername1234567890',
+    textLength: 10,
+};
+
 export const SmallAvatar = Template.bind({});
 SmallAvatar.args = {
-    src: 'https://via.placeholder.com/150',
+    src: 'https://img.freepik.com/premium-vector/vector-flat-illustration-grayscale-avatar-user-profile-person-icon-profile-picture-business-profile-woman-suitable-social-media-profiles-icons-screensavers-as-templatex9_719432-1339.jpg',
     alt: 'Small Avatar',
     size: 50,
 };
 
 export const LargeAvatar = Template.bind({});
 LargeAvatar.args = {
-    src: 'https://via.placeholder.com/150',
+    src: 'https://img.freepik.com/premium-vector/vector-flat-illustration-grayscale-avatar-user-profile-person-icon-profile-picture-business-profile-woman-suitable-social-media-profiles-icons-screensavers-as-templatex9_719432-1339.jpg',
     alt: 'Large Avatar',
     size: 150,
 };
