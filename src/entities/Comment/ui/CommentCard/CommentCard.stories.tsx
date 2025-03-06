c7ce60a2649b5e795eb2362815be3afcf942c723
Maryna Shavlak <shavlakmaryna@gmail.com>
2025-03-06 11:15:22 +0200

    fix CommentCard storybook

--- src/entities/Comment/ui/CommentCard/CommentCard.stories.tsx
+++ src/entities/Comment/ui/CommentCard/CommentCard.stories.tsx
@@ -17,12 +17,30 @@ const Template: ComponentStory<typeof CommentCard> = (args) => (
     <CommentCard {...args} />
 );
 
+// Mock deleteComment function
+const mockDeleteComment = async (commentId: string) => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log(`Comment with ID ${commentId} deleted.`);
+            resolve(true);
+        }, 500);
+    });
+};
+
 const normalArgs = {
     comment: testCommentData,
+    canDeleteComments: false,
+};
+
+const withDeleteArgs = {
+    comment: testCommentData,
+    canDeleteComments: true,
+    deleteComment: mockDeleteComment,
 };
 
 const noUserAvatarArgs = {
     comment: testCommentNoUserAvatarData,
+    canDeleteComments: false,
 };
 
 const loadingArgs = {
@@ -31,6 +49,9 @@ const loadingArgs = {
 
 export const WithUserAvatar = Template.bind({});
 WithUserAvatar.args = normalArgs;
+
+export const WithUserAvatarCanDelete = Template.bind({});
+WithUserAvatarCanDelete.args = withDeleteArgs;
 
 export const NoUserAvatar = Template.bind({});
 NoUserAvatar.args = noUserAvatarArgs;
@@ -41,6 +62,10 @@ Loading.args = loadingArgs;
 export const WithUserAvatarRedesigned = Template.bind({});
 WithUserAvatarRedesigned.args = normalArgs;
 WithUserAvatarRedesigned.decorators = [NewDesignDecorator];
+
+export const WithUserAvatarCanDeleteRedesigned = Template.bind({});
+WithUserAvatarCanDeleteRedesigned.args = withDeleteArgs;
+WithUserAvatarCanDeleteRedesigned.decorators = [NewDesignDecorator];
 
 export const NoUserAvatarRedesigned = Template.bind({});
 NoUserAvatarRedesigned.args = noUserAvatarArgs;
