e079fdb7482ca31a6d085c1784f913a3636e5e8e
Maryna Shavlak <shavlakmaryna@gmail.com>
2025-03-06 17:31:55 +0200

    add storybook for ArticleDetails

--- src/entities/Article/ui/ArticleDetails/ArticleDetails.stories.tsx
+++ src/entities/Article/ui/ArticleDetails/ArticleDetails.stories.tsx
@@ -1,83 +1,53 @@
-export {};
-// import React from 'react';
-// import { ComponentMeta, ComponentStory } from '@storybook/react';
-// import { StoreDecorator } from '@/shared/config/storybook/StoreDecorator/StoreDecorator';
-//
-// import { ArticleDetails } from './ArticleDetails';
-// import { testArticleData } from '../../testing';
-// import { NewDesignDecorator } from '@/shared/config/storybook/NewDesignDecorator/NewDesignDecorator';
-//
-// export default {
-//     title: 'entities/Article/ArticleDetails',
-//     component: ArticleDetails,
-//     argTypes: {
-//         backgroundColor: { control: 'color' },
-//     },
-// } as ComponentMeta<typeof ArticleDetails>;
-//
-// const Template: ComponentStory<typeof ArticleDetails> = (args) => (
-//     <ArticleDetails {...args} />
-// );
-//
-// export const Normal = Template.bind({});
-// Normal.args = {};
-// Normal.decorators = [
-//     StoreDecorator({
-//         articleDetails: {
-//             data: testArticleData,
-//         },
-//     }),
-// ];
-//
-// export const Loading = Template.bind({});
-// Loading.args = {};
-// Loading.decorators = [
-//     StoreDecorator({
-//         articleDetails: {
-//             isLoading: true,
-//         },
-//     }),
-// ];
-//
-// export const Error = Template.bind({});
-// Error.args = {};
-// Error.decorators = [
-//     StoreDecorator({
-//         articleDetails: {
-//             error: 'error',
-//         },
-//     }),
-// ];
-//
-// export const NormalRedesigned = Template.bind({});
-// NormalRedesigned.args = {};
-// NormalRedesigned.decorators = [
-//     StoreDecorator({
-//         articleDetails: {
-//             data: testArticleData,
-//         },
-//     }),
-//     NewDesignDecorator,
-// ];
-//
-// export const LoadingRedesigned = Template.bind({});
-// LoadingRedesigned.args = {};
-// LoadingRedesigned.decorators = [
-//     StoreDecorator({
-//         articleDetails: {
-//             isLoading: true,
-//         },
-//     }),
-//     NewDesignDecorator,
-// ];
-//
-// export const ErrorRedesigned = Template.bind({});
-// ErrorRedesigned.args = {};
-// ErrorRedesigned.decorators = [
-//     StoreDecorator({
-//         articleDetails: {
-//             error: 'error',
-//         },
-//     }),
-//     NewDesignDecorator,
-// ];
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { ArticleDetails } from './ArticleDetails';
+import { NewDesignDecorator } from '@/shared/config/storybook/NewDesignDecorator/NewDesignDecorator';
+import { StoreDecorator } from '@/shared/config/storybook/StoreDecorator/StoreDecorator';
+import { ArticleDetailsSkeleton } from './ArticleDetailsSkeleton/ArticleDetailsSkeleton';
+import { ArticleDetailsError } from './ArticleDetailsError/ArticleDetailsError';
+
+export default {
+    title: 'entities/Article/ArticleDetails',
+    component: ArticleDetails,
+    argTypes: {
+        backgroundColor: { control: 'color' },
+    },
+    decorators: [StoreDecorator({})],
+} as ComponentMeta<typeof ArticleDetails>;
+
+const Template: ComponentStory<typeof ArticleDetails> = (args) => (
+    <ArticleDetails {...args} />
+);
+
+const normalArgs = {
+    id: '96',
+};
+
+export const Default = Template.bind({});
+Default.args = normalArgs;
+
+export const DefaultRedesigned = Template.bind({});
+DefaultRedesigned.args = normalArgs;
+DefaultRedesigned.decorators = [NewDesignDecorator];
+
+export const LoadingState = Template.bind({});
+LoadingState.args = normalArgs;
+LoadingState.decorators = [() => <ArticleDetailsSkeleton />];
+
+export const LoadingStateRedesigned = Template.bind({});
+LoadingStateRedesigned.args = normalArgs;
+LoadingStateRedesigned.decorators = [
+    () => <ArticleDetailsSkeleton />,
+    NewDesignDecorator,
+];
+
+export const ErrorState = Template.bind({});
+ErrorState.args = normalArgs;
+ErrorState.decorators = [() => <ArticleDetailsError />];
+
+export const ErrorStateRedesigned = Template.bind({});
+ErrorStateRedesigned.args = normalArgs;
+ErrorStateRedesigned.decorators = [
+    () => <ArticleDetailsError />,
+    NewDesignDecorator,
+];
--- src/entities/Article/ui/ArticleDetails/ArticleDetails.tsx
+++ src/entities/Article/ui/ArticleDetails/ArticleDetails.tsx
@@ -7,37 +7,13 @@ export interface ArticleDetailsProps {
     id?: string;
 }
 
-// const reducers: ReducersList = {
-//     articleDetails: articleDetailsReducer,
-// };
-
 export const ArticleDetails = memo((props: ArticleDetailsProps) => {
     const { id } = props;
-
-    // useInitialEffect(() => {
-    //     dispatch(fetchArticleByIdThunk(id));
-    // });
-
-    // useEffect(() => {
-    //     if (__PROJECT__ !== 'storybook' && __PROJECT__ !== 'jest') {
-    //         if (id) {
-    //             const action = dispatch(fetchArticleByIdThunk(id));
-    //             return () => {
-    //                 action.abort();
-    //             };
-    //         }
-    //     }
-    //
-    //     return undefined;
-    // }, [dispatch, id]);
-
     return (
-        // <DynamicModuleLoader reducers={reducers}>
         <ToggleFeaturesComponent
             feature="isAppRedesigned"
             on={<ArticleDetailsRedesigned id={id} />}
             off={<ArticleDetailsDeprecated id={id} />}
         />
-        // </DynamicModuleLoader>
     );
 });
