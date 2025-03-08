eecacbf1b30f7c62dce0a2905218d3ba3438e7f5
Maryna Shavlak <shavlakmaryna@gmail.com>
2025-03-08 14:00:59 +0200

    add new stories

--- src/features/ArticleComments/ui/ArticleComments.stories.tsx
+++ src/features/ArticleComments/ui/ArticleComments.stories.tsx
@@ -3,14 +3,13 @@ import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { StoreDecorator } from '@/shared/config/storybook/StoreDecorator/StoreDecorator';
 import { ArticleComments } from '..';
 import { NewDesignDecorator } from '@/shared/config/storybook/NewDesignDecorator/NewDesignDecorator';
-import { testCommentsData } from '@/entities/Comment/testing';
 
 export default {
     title: 'features/ArticleComments',
     component: ArticleComments,
-    args: {
-        id: '1',
-    },
+    // args: {
+    //     id: '1',
+    // },
     argTypes: {
         backgroundColor: { control: 'color' },
     },
@@ -20,16 +19,16 @@ const Template: ComponentStory<typeof ArticleComments> = (args) => (
     <ArticleComments {...args} />
 );
 
-const normalData = {
-    ids: testCommentsData.map((comment) => comment.id),
-    entities: testCommentsData.reduce((acc, comment) => {
-        // @ts-ignore
-        acc[comment.id] = comment;
-        return acc;
-    }, {}),
-    isLoading: false,
-    error: undefined,
-};
+// const normalData = {
+//     ids: testCommentsData.map((comment) => comment.id),
+//     entities: testCommentsData.reduce((acc, comment) => {
+//         // @ts-ignore
+//         acc[comment.id] = comment;
+//         return acc;
+//     }, {}),
+//     isLoading: false,
+//     error: undefined,
+// };
 
 const withNoInitialCommentsData = {
     ids: [],
@@ -51,66 +50,45 @@ const errorData = {
     error: 'Failed to load comments',
 };
 
+const normalArgs = { id: '1' };
+
 export const Normal = Template.bind({});
-Normal.args = {};
-Normal.decorators = [
-    StoreDecorator({
-        articleComments: normalData,
-    }),
-];
+Normal.args = normalArgs;
+Normal.decorators = [StoreDecorator({})];
 
 export const NormalRedesigned = Template.bind({});
-NormalRedesigned.args = {};
-NormalRedesigned.decorators = [
-    StoreDecorator({ articleComments: normalData }),
-    NewDesignDecorator,
-];
+NormalRedesigned.args = normalArgs;
+NormalRedesigned.decorators = [StoreDecorator({}), NewDesignDecorator];
 
 export const WithNoInitialComments = Template.bind({});
-WithNoInitialComments.args = {};
-WithNoInitialComments.decorators = [
-    StoreDecorator({
-        articleComments: withNoInitialCommentsData,
-    }),
-];
+WithNoInitialComments.args = { id: '2' };
+WithNoInitialComments.decorators = [StoreDecorator({})];
 
 export const WithNoInitialCommentsRedesigned = Template.bind({});
-WithNoInitialCommentsRedesigned.args = {};
+WithNoInitialCommentsRedesigned.args = { id: '2' };
 WithNoInitialCommentsRedesigned.decorators = [
-    StoreDecorator({ articleComments: withNoInitialCommentsData }),
+    StoreDecorator({}),
     NewDesignDecorator,
 ];
 
-export const Loading = Template.bind({});
-Loading.args = {};
-Loading.decorators = [
-    StoreDecorator({
-        articleComments: loadingData,
-    }),
-];
+export const LoadingState = Template.bind({});
+LoadingState.args = normalArgs;
+LoadingState.decorators = [() => <NotificationListSkeleton />];
 
-export const LoadingRedesigned = Template.bind({});
-LoadingRedesigned.args = {};
-LoadingRedesigned.decorators = [
-    StoreDecorator({
-        articleComments: loadingData,
-    }),
+export const LoadingStateRedesigned = Template.bind({});
+LoadingStateRedesigned.args = normalArgs;
+LoadingStateRedesigned.decorators = [
+    () => <NotificationListSkeleton />,
     NewDesignDecorator,
 ];
 
-export const Error = Template.bind({});
-Error.args = {};
-Error.decorators = [
-    StoreDecorator({
-        articleComments: errorData,
-    }),
-];
+export const ErrorStateArticleComments = Template.bind({});
+ErrorStateArticleComments.args = normalArgs;
+ErrorStateArticleComments.decorators = [() => <ErrorNotificationsList />];
 
-export const ErrorRedesigned = Template.bind({});
-ErrorRedesigned.args = {};
-ErrorRedesigned.decorators = [
-    StoreDecorator({
-        articleComments: errorData,
-    }),
+export const ErrorStateArticleCommentsRedesigned = Template.bind({});
+ErrorStateArticleCommentsRedesigned.args = normalArgs;
+ErrorStateArticleCommentsRedesigned.decorators = [
+    () => <ErrorNotificationsList />,
     NewDesignDecorator,
 ];
