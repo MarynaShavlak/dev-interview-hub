78589965c0e9b5a5e2a175dd2b44701d97370a1e
Maryna Shavlak <shavlakmaryna@gmail.com>
2025-03-10 16:20:40 +0200

    storybook for Signup

---
+++ src/features/AuthUser/ui/AuthForm/SignupForm/SignupForm.stories.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { SignupForm } from './SignupForm';
+
+import { StoreDecorator } from '@/shared/config/storybook/StoreDecorator/StoreDecorator';
+import { NewDesignDecorator } from '@/shared/config/storybook/NewDesignDecorator/NewDesignDecorator';
+
+export default {
+    title: 'features/Auth/SignupForm',
+    component: SignupForm,
+    argTypes: {
+        backgroundColor: { control: 'color' },
+    },
+    decorators: [StoreDecorator({})],
+} as ComponentMeta<typeof SignupForm>;
+
+const Template: ComponentStory<typeof SignupForm> = (args) => (
+    <SignupForm {...args} />
+);
+
+const normalArgs = {
+    onSuccess: () => console.log('Signup successful'),
+};
+
+export const Default = Template.bind({});
+Default.args = normalArgs;
+
+export const DefaultRedesigned = Template.bind({});
+DefaultRedesigned.args = normalArgs;
+DefaultRedesigned.decorators = [NewDesignDecorator];
