382c6c2f3a5d62388bbfad11e7ebaf96890b06b0
Maryna Shavlak <shavlakmaryna@gmail.com>
2025-03-11 14:45:41 +0200

    add storybok for features/Table/Header/FilterTrigger

---
+++ src/features/Table/ui/TableHeader/TableHeaderCell/TableFilter/FilterTrigger/FilterTrigger.stories.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { FilterTrigger } from './FilterTrigger';
+import { NewDesignDecorator } from '@/shared/config/storybook/NewDesignDecorator/NewDesignDecorator';
+
+export default {
+    title: 'features/Table/Header/FilterTrigger',
+    component: FilterTrigger,
+    argTypes: {
+        isFilterActive: { control: 'boolean' },
+    },
+} as ComponentMeta<typeof FilterTrigger>;
+
+const Template: ComponentStory<typeof FilterTrigger> = (args) => (
+    <FilterTrigger {...args} />
+);
+
+export const Inactive = Template.bind({});
+Inactive.args = {
+    isFilterActive: false,
+};
+
+export const Active = Template.bind({});
+Active.args = {
+    isFilterActive: true,
+};
+
+export const InactiveRedesigned = Template.bind({});
+InactiveRedesigned.args = { isFilterActive: false };
+InactiveRedesigned.decorators = [NewDesignDecorator];
+
+export const ActiveRedesigned = Template.bind({});
+ActiveRedesigned.args = { isFilterActive: true };
+ActiveRedesigned.decorators = [NewDesignDecorator];
