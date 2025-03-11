963ed46aa64a69d2ddbc8aa2ef2909ed8e68e4d5
Maryna Shavlak <shavlakmaryna@gmail.com>
2025-03-11 13:59:52 +0200

    add code for empty table

---
+++ src/features/Table/ui/EmptyTableState/EmptyTableState.stories.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { withI18nDecorator } from '@/shared/config/storybook/withI18nDecorator/withI18nDecorator';
+import { EmptyTableState } from '../..';
+import { NewDesignDecorator } from '@/shared/config/storybook/NewDesignDecorator/NewDesignDecorator';
+
+export default {
+    title: 'features/Table/EmptyTableState',
+    component: EmptyTableState,
+    argTypes: {
+        backgroundColor: { control: 'color' },
+    },
+    decorators: [withI18nDecorator],
+} as ComponentMeta<typeof EmptyTableState>;
+
+const Template: ComponentStory<typeof EmptyTableState> = (args) => (
+    <EmptyTableState {...args} />
+);
+
+export const Default = Template.bind({});
+Default.args = {
+    message: 'Не створено жодної статті',
+};
+
+export const DefaultRedesigned = Template.bind({});
+DefaultRedesigned.args = {
+    message: 'Не створено жодної статті',
+};
+DefaultRedesigned.decorators = [NewDesignDecorator];
