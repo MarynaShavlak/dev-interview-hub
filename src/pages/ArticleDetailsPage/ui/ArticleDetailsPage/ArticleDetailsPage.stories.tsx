71c6935753fe34a0c57003698430bacabee6ae62
Maryna Shavlak <shavlakmaryna@gmail.com>
2025-03-13 14:34:17 +0200

    add storybook for page

--- src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.stories.tsx
+++ src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.stories.tsx
@@ -4,11 +4,16 @@ import ArticleDetailsPage from './ArticleDetailsPage';
 import { StoreDecorator } from '@/shared/config/storybook/StoreDecorator/StoreDecorator';
 import { withI18nDecorator } from '@/shared/config/storybook/withI18nDecorator/withI18nDecorator';
 import { NewDesignDecorator } from '@/shared/config/storybook/NewDesignDecorator/NewDesignDecorator';
+import { ArticleDetailsPageSkeleton } from '../..';
+import { UserRole } from '@/entities/User';
 
 export default {
     title: 'pages/ArticleDetailsPage/ArticleDetailsPage',
     component: ArticleDetailsPage,
-    decorators: [StoreDecorator({}), withI18nDecorator],
+    decorators: [
+        StoreDecorator({ user: { authData: { roles: [UserRole.ADMIN] } } }),
+        withI18nDecorator,
+    ],
     argTypes: {
         backgroundColor: { control: 'color' },
     },
@@ -28,3 +33,11 @@ const Template: ComponentStory<typeof ArticleDetailsPage> = (args) => (
 export const Default = Template.bind({});
 export const DefaultRedesigned = Template.bind({});
 DefaultRedesigned.decorators = [NewDesignDecorator];
+
+export const Loading = Template.bind({});
+Loading.decorators = [() => <ArticleDetailsPageSkeleton />];
+export const LoadingRedesigned = Template.bind({});
+LoadingRedesigned.decorators = [
+    () => <ArticleDetailsPageSkeleton />,
+    NewDesignDecorator,
+];
--- src/pages/ArticleDetailsPage/ui/ArticleDetailsPageSkeleton/ArticleDetailsPageSkeleton.tsx
+++ src/pages/ArticleDetailsPage/ui/ArticleDetailsPageSkeleton/ArticleDetailsPageSkeleton.tsx
@@ -1,14 +1,49 @@
 import { memo } from 'react';
 import { AdditionalInfoContainerSkeleton } from '../AdditionalInfoContainer/AdditionalInfoContainerSkeleton/AdditionalInfoContainerSkeleton';
 import { StickyContentLayout } from '@/shared/layouts/StickyContentLayout';
-import { Skeleton } from '@/shared/ui/redesigned/Skeleton';
+import { Skeleton as SkeletonRedesigned } from '@/shared/ui/redesigned/Skeleton';
+import { toggleFeatures, ToggleFeaturesComponent } from '@/shared/lib/features';
+import { Skeleton as SkeletonDeprecated } from '@/shared/ui/deprecated/Skeleton';
+import { HStack, VStack } from '@/shared/ui/common/Stack';
+import cls from '../ArticleDetailsPage/ArticleDetailsPage.module.scss';
 
 export const ArticleDetailsPageSkeleton = memo(() => {
+    const Skeleton = toggleFeatures({
+        name: 'isAppRedesigned',
+        on: () => SkeletonRedesigned,
+        off: () => SkeletonDeprecated,
+    });
     return (
-        <StickyContentLayout
-            left={<Skeleton width="120px" height="40px" border="34px" />}
-            right={<AdditionalInfoContainerSkeleton />}
-            content={<Skeleton width="100%" height="100vh" border="40px" />}
+        <ToggleFeaturesComponent
+            feature="isAppRedesigned"
+            on={
+                <StickyContentLayout
+                    left={
+                        <Skeleton width="120px" height="40px" border="34px" />
+                    }
+                    right={<AdditionalInfoContainerSkeleton />}
+                    content={
+                        <Skeleton width="100%" height="100vh" border="40px" />
+                    }
+                />
+            }
+            off={
+                <VStack gap="16">
+                    <HStack max justify="between">
+                        <Skeleton width="164px" height="38px" />
+                        <Skeleton width="182px" height="38px" />
+                    </HStack>
+                    <HStack
+                        max
+                        justify="center"
+                        align="center"
+                        className={cls.avatarWrap}
+                    >
+                        <Skeleton width="175px" height="175px" border="50%" />
+                    </HStack>
+                    <Skeleton width="100%" height="300px" />
+                </VStack>
+            }
         />
     );
 });
