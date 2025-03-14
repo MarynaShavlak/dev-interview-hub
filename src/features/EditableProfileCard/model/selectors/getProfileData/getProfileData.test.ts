c195d4cecf7abc9e4a0ed1672235b88c21d57467
Maryna Shavlak <shavlakmaryna@gmail.com>
2025-03-14 18:18:02 +0200

    fix some tests

--- src/features/EditableProfileCard/model/selectors/getProfileData/getProfileData.test.ts
+++ src/features/EditableProfileCard/model/selectors/getProfileData/getProfileData.test.ts
@@ -1,20 +1,18 @@
-// import { StateSchema } from '@/app/providers/StoreProvider';
-// import { getProfileData } from './getProfileData';
-// import { testProfileData } from '@/entities/Profile/testing';
-//
-// describe('getProfileData', () => {
-//     test('should return profile data', () => {
-//         const state: DeepPartial<StateSchema> = {
-//             profile: {
-//                 data: testProfileData,
-//             },
-//         };
-//         expect(getProfileData(state as StateSchema)).toEqual(testProfileData);
-//     });
-//     test('should work with empty state', () => {
-//         const state: DeepPartial<StateSchema> = {};
-//         expect(getProfileData(state as StateSchema)).toEqual(undefined);
-//     });
-// });
+import { StateSchema } from '@/app/providers/StoreProvider';
+import { getProfileData } from './getProfileData';
+import { testUserData } from '@/entities/User/testing';
 
-export {};
+describe('getProfileData', () => {
+    test('should return profile data', () => {
+        const state: DeepPartial<StateSchema> = {
+            profile: {
+                data: testUserData,
+            },
+        };
+        expect(getProfileData(state as StateSchema)).toEqual(testUserData);
+    });
+    test('should work with empty state', () => {
+        const state: DeepPartial<StateSchema> = {};
+        expect(getProfileData(state as StateSchema)).toEqual(undefined);
+    });
+});
--- src/features/EditableProfileCard/model/selectors/getProfileForm/getProfileForm.test.ts
+++ src/features/EditableProfileCard/model/selectors/getProfileForm/getProfileForm.test.ts
@@ -1,19 +1,18 @@
-// import { StateSchema } from '@/app/providers/StoreProvider';
-// import { getProfileForm } from './getProfileForm';
-// import { testProfileData } from '@/entities/Profile/testing';
-//
-// describe('getProfileForm', () => {
-//     test('should return profile form data', () => {
-//         const state: DeepPartial<StateSchema> = {
-//             profile: {
-//                 form: testProfileData,
-//             },
-//         };
-//         expect(getProfileForm(state as StateSchema)).toEqual(testProfileData);
-//     });
-//     test('should work with empty state', () => {
-//         const state: DeepPartial<StateSchema> = {};
-//         expect(getProfileForm(state as StateSchema)).toEqual(undefined);
-//     });
-// });
-export {};
+import { StateSchema } from '@/app/providers/StoreProvider';
+import { getProfileForm } from './getProfileForm';
+import { testUserData } from '@/entities/User/testing';
+
+describe('getProfileForm', () => {
+    test('should return profile form data', () => {
+        const state: DeepPartial<StateSchema> = {
+            profile: {
+                form: testUserData,
+            },
+        };
+        expect(getProfileForm(state as StateSchema)).toEqual(testUserData);
+    });
+    test('should work with empty state', () => {
+        const state: DeepPartial<StateSchema> = {};
+        expect(getProfileForm(state as StateSchema)).toEqual(undefined);
+    });
+});
