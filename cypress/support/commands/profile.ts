ffb189719d7b9f992deefee20e07f4ed5f3ac1c3
Maryna Shavlak <shavlakmaryna@gmail.com>
2025-03-20 16:28:48 +0200

    add code for fixes in tests e2e

--- cypress/support/commands/profile.ts
+++ cypress/support/commands/profile.ts
@@ -1,13 +1,5 @@
-import {
-    collection,
-    doc,
-    getDocs,
-    query,
-    setDoc,
-    where,
-} from 'firebase/firestore';
-import { firestore } from '../../../json-server/firebase';
 import { User } from '../../../src/entities/User';
+import { resetUserProfileInFirestore } from '../../helpers/resetUserProfileInFirestore';
 
 export const updateProfile = (firstname: string, lastname: string) => {
     cy.getByTestId('EditableProfileCardHeader.EditButton').click();
@@ -45,27 +37,12 @@ export const resetProfile = (profileId: string) => {
         city: 'Kharkiv',
         username: 'andrii_shavlak',
     };
-
-    return cy.wrap(null).then(async () => {
-        try {
-            // Query to find the document where 'id' field matches profileId
-            const usersRef = collection(firestore, 'users');
-            const q = query(usersRef, where('id', '==', profileId));
-            const querySnapshot = await getDocs(q);
-
-            if (querySnapshot.empty) {
-                throw new Error(`No user found with id: ${profileId}`);
-            }
-
-            const userDoc = querySnapshot.docs[0];
-            const userRef = doc(firestore, 'users', userDoc.id);
-
-            await setDoc(userRef, defaultProfileData, { merge: false });
-
-            return defaultProfileData;
-        } catch (error) {
-            throw new Error(`Failed to reset profile in Firestore: ${error}`);
-        }
+    return cy.wrap(null).then(() => {
+        return resetUserProfileInFirestore(profileId, defaultProfileData)
+            .then((resetData) => resetData)
+            .catch((error) => {
+                throw new Error(error.message);
+            });
     });
 };
 
