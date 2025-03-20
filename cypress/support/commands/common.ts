c540397326ba15ca7b47374c8fc4e62a376ec1d8
Maryna Shavlak <shavlakmaryna@gmail.com>
2025-03-20 13:41:54 +0200

    fix code for e2e tests

--- cypress/support/commands/common.ts
+++ cypress/support/commands/common.ts
@@ -1,25 +1,29 @@
+import { signInWithEmailAndPassword } from 'firebase/auth';
 import { USER_LOCALSTORAGE_KEY } from '../../../src/shared/const/localstorage';
 import { User } from '../../../src/entities/User';
 import { selectByTestId } from '../../helpers/selectByTestId';
+import { auth } from '../../../json-server/firebase';
 
 export const login = (
-    username: string = 'testuser',
-    password: string = '123',
+    email: string = 'andrii_shavlak@gmail.com', // Default email
+    password: string = 'andrii_shavlak2908', // Default password
 ) => {
-    return cy
-        .request({
-            method: 'POST',
-            url: `${Cypress.env('apiUrl')}/login`,
-            body: {
-                username,
-                password,
-            },
-        })
-        .then(({ body }) => {
-            console.log('body', body);
-            window.localStorage.setItem(USER_LOCALSTORAGE_KEY, body.id);
-            return body;
-        });
+    return cy.wrap(null).then(() => {
+        return signInWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                const firebaseUser = userCredential.user;
+
+                window.localStorage.setItem(
+                    USER_LOCALSTORAGE_KEY,
+                    firebaseUser.uid,
+                );
+
+                return firebaseUser;
+            })
+            .catch((error) => {
+                throw new Error(`Firebase login failed: ${error.message}`);
+            });
+    });
 };
 
 export const getByTestId = (testId: string) => {
