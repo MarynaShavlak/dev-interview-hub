2fb95b9e3a8417325d9c2a1ec9843b52c06e16a7
Maryna Shavlak <shavlakmaryna@gmail.com>
2025-03-30 20:03:13 +0300

    fix manifest

--- config/serviceWorker/config-sw.js
+++ config/serviceWorker/config-sw.js
@@ -11,8 +11,6 @@ clientsClaim();
 self.skipWaiting();
 precacheAndRoute(self.__WB_MANIFEST);
 
-// Cache the Google Fonts stylesheets with a stale-while-revalidate strategy.
-// @see https://developers.google.com/web/tools/workbox/guides/common-recipes#google_fonts
 registerRoute(
     ({ url }) => url.origin === 'https://fonts.googleapis.com',
     new StaleWhileRevalidate({
@@ -20,8 +18,6 @@ registerRoute(
     }),
 );
 
-// Cache the underlying font files with a cache-first strategy for 1 year.
-// @see https://developers.google.com/web/tools/workbox/guides/common-recipes#google_fonts
 registerRoute(
     ({ url }) => url.origin === 'https://fonts.gstatic.com',
     new CacheFirst({
@@ -54,12 +50,6 @@ registerRoute(
     }),
 );
 
-/**
- * We use CacheFirst for images because, images are not going to change very often,
- * so it does not make sense to revalidate images on every request.
- *
- * @see https://developers.google.com/web/tools/workbox/guides/common-recipes#caching_images
- */
 registerRoute(
     ({ request }) => request.destination === 'image',
     new CacheFirst({
@@ -76,7 +66,6 @@ registerRoute(
     }),
 );
 
-// @see https://developers.google.com/web/tools/workbox/guides/common-recipes#cache_css_and_javascript_files
 registerRoute(
     ({ request }) =>
         request.destination === 'script' || request.destination === 'style',
--- config/serviceWorker/serviceWorkerRegistration.js
+++ config/serviceWorker/serviceWorkerRegistration.js
@@ -1,32 +1,3 @@
-// export default function registerServiceWorker() {
-//     // if ( 'production' !== process.env.NODE_ENV ) {
-//     //     return;
-//     // }
-//
-//     if (process.env.mode !== 'production') {
-//         return;
-//     }
-//     // Check if the serviceWorker Object exists in the navigator object ( means if browser supports SW )
-//     if ('serviceWorker' in navigator) {
-//         const wb = new Workbox('./sw.js');
-//
-//         wb.addEventListener('installed', (event) => {
-//             /**
-//              * We have the condition - event.isUpdate because we don't want to show
-//              * this message on the very first service worker installation,
-//              * only on the updated
-//              */
-//             if (event.isUpdate) {
-//                 if (
-//                     confirm(`New app update is available!. Click OK to refresh`)
-//                 ) {
-//                     window.location.reload();
-//                 }
-//             }
-//         });
-//         wb.register();
-//     }
-// }
 import { Workbox } from 'workbox-window';
 
 export default function registerServiceWorker() {
@@ -35,7 +6,6 @@ export default function registerServiceWorker() {
         process.env.NODE_ENV,
     );
 
-    // Check both NODE_ENV and mode to be safe
     if (
         process.env.NODE_ENV !== 'production' &&
         process.env.mode !== 'production'
