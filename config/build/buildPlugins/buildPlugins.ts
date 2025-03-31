e9afd160a992c4fa1ae4056589da8ddaf7d39828
Maryna Shavlak <shavlakmaryna@gmail.com>
2025-03-31 13:09:31 +0300

    add manifest

--- config/build/buildPlugins/buildPlugins.ts
+++ config/build/buildPlugins/buildPlugins.ts
@@ -4,7 +4,6 @@ import webpack from 'webpack';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
-import CopyPlugin from 'copy-webpack-plugin';
 import CircularDependencyPlugin from 'circular-dependency-plugin';
 import { InjectManifest } from 'workbox-webpack-plugin';
 import  Dotenv from 'dotenv-webpack';
@@ -29,12 +28,42 @@ export const buildPlugins = ({
             mode: 'webapp',
             devMode: 'webapp',
             prefix: 'assets/favicons/',
+            manifest: 'src/manifest.json',
             cache: true,
             inject: true,
             favicons: {
                 background: '#fff',
-                theme_color: '#333',
-            }
+                theme_color: '#fff',
+                start_url: '../../',
+                appleStatusBarStyle: 'default',
+                icons: {
+                    "android": [
+                        "android-chrome-144x144.png",
+                        "android-chrome-192x192.png",
+                        "android-chrome-256x256.png",
+                        "android-chrome-36x36.png",
+                        "android-chrome-384x384.png",
+                        "android-chrome-48x48.png",
+                        "android-chrome-512x512.png",
+                        "android-chrome-72x72.png",
+                        "android-chrome-96x96.png"
+                    ],
+                    "appleIcon": [
+                                 "apple-touch-icon-180x180.png",
+
+                        "apple-touch-icon-precomposed.png",
+                        "apple-touch-icon.png",
+                    ],
+                    "appleStartup": false,
+                    "favicons": [
+                          "favicon.ico",
+                    ],
+                    "windows": false,
+                    "yandex": false,
+
+                },
+            },
+
         }),
         new webpack.ProgressPlugin(),
         new webpack.DefinePlugin({
@@ -74,15 +103,15 @@ export const buildPlugins = ({
             filename: 'css/[name].[contenthash:8].css',
             chunkFilename: 'css/[name].[contenthash:8].css',
         }));
-        plugins.push(new CopyPlugin({
-            patterns: [
-                { from: paths.locales, to: paths.buildLocales },
-                { from: `${paths.src}/favicon.ico`, to: paths.build },
-                { from: `${paths.src}/manifest.json`, to: paths.build  },
-                { from: `${paths.src}/logo192.png`, to: paths.build  },
-                { from: `${paths.src}/logo512.png`, to: paths.build  },
-            ],
-        }));
+        // plugins.push(new CopyPlugin({
+        //     patterns: [
+        //         { from: paths.locales, to: paths.buildLocales },
+        //         { from: `${paths.src}/favicon.ico`, to: paths.build },
+        //         { from: `${paths.src}/manifest.json`, to: paths.build  },
+        //         { from: `${paths.src}/logo192.png`, to: paths.build  },
+        //         { from: `${paths.src}/logo512.png`, to: paths.build  },
+        //     ],
+        // }));
         plugins.push(new InjectManifest( {
             swSrc: './config/serviceWorker/config-sw.js',
                       swDest: `${paths.build}/sw.js`,
