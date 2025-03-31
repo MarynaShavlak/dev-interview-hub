21fafb0ef70e7bbe37b70a2fa372ed6b47f65220
Maryna Shavlak <shavlakmaryna@gmail.com>
2025-03-31 13:21:27 +0300

    fix sw code

--- config/serviceWorker/serviceWorkerRegistration.js
+++ config/serviceWorker/serviceWorkerRegistration.js
@@ -19,39 +19,39 @@ export default function registerServiceWorker() {
         console.log('Service Worker is supported by this browser');
         const wb = new Workbox('./sw.js');
 
-        wb.addEventListener('installed', (event) => {
-            console.log(
-                'Service worker installed',
-                event.isUpdate ? '(update)' : '(first install)',
-            );
-            if (event.isUpdate) {
-                if (
-                    confirm(`New app update is available! Click OK to refresh`)
-                ) {
-                    window.location.reload();
-                }
-            }
-        });
+        // wb.addEventListener('installed', (event) => {
+        //     console.log(
+        //         'Service worker installed',
+        //         event.isUpdate ? '(update)' : '(first install)',
+        //     );
+        //     if (event.isUpdate) {
+        //         if (
+        //             confirm(`New app update is available! Click OK to refresh`)
+        //         ) {
+        //             window.location.reload();
+        //         }
+        //     }
+        // });
 
-        wb.addEventListener('activated', () => {
-            console.log('Service worker activated');
-        });
-
-        wb.addEventListener('waiting', () => {
-            console.log('Service worker waiting');
-        });
+        // wb.addEventListener('activated', () => {
+        //     console.log('Service worker activated');
+        // });
+        //
+        // wb.addEventListener('waiting', () => {
+        //     console.log('Service worker waiting');
+        // });
 
         wb.register()
             .then((registration) => {
-                console.log(
-                    'Service Worker registered with scope:',
-                    registration.scope,
-                );
+                // console.log(
+                //     'Service Worker registered with scope:',
+                //     registration.scope,
+                // );
             })
             .catch((error) => {
-                console.error('Service Worker registration failed:', error);
+                // console.error('Service Worker registration failed:', error);
             });
     } else {
-        console.log('Service Worker is not supported by this browser');
+        // console.log('Service Worker is not supported by this browser');
     }
 }
