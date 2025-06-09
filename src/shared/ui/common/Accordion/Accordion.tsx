7b9591169f9e38388cb6c0f49502c9ee4260bee2
Maryna Shavlak <shavlakmaryna@gmail.com>
2025-06-09 20:43:15 +0300

    add deprecated Accordion

--- src/shared/ui/common/Accordion/Accordion.tsx
+++ src/shared/ui/common/Accordion/Accordion.tsx
@@ -1,9 +1,11 @@
 import * as RadixAccordion from '@radix-ui/react-accordion';
 import { forwardRef, ReactNode } from 'react';
-import ArrowDownIcon from '@/shared/assets/icons/arrow-bottom.svg';
 import cls from './Accordion.module.scss';
-import { Icon } from '../../redesigned/Icon';
 import { classNames } from '@/shared/lib/classes/classNames/classNames';
+import { toggleFeatures, ToggleFeaturesComponent } from '@/shared/lib/features';
+import ArrowDownIcon from '@/shared/assets/icons/arrow-bottom.svg';
+import { Icon } from '../../redesigned/Icon';
+import { Icon as IconDeprecated } from '../../deprecated/Icon';
 
 interface AccordionItem {
     trigger: ReactNode;
@@ -40,23 +42,52 @@ interface TriggerProps extends RadixAccordion.AccordionTriggerProps {
 }
 
 const AccordionTrigger = forwardRef<HTMLButtonElement, TriggerProps>(
-    ({ children, className, ...props }: TriggerProps, forwardedRef) => (
-        <RadixAccordion.Header className={cls.header}>
-            <RadixAccordion.Trigger
-                className={cls.trigger}
-                {...props}
-                ref={forwardedRef}
-            >
-                {children}
-                <Icon
-                    aria-hidden
-                    Svg={ArrowDownIcon}
-                    {...ICON_PROPS}
-                    className={cls.icon}
-                />
-            </RadixAccordion.Trigger>
-        </RadixAccordion.Header>
-    ),
+    ({ children, className, ...props }: TriggerProps, forwardedRef) => {
+        const triggerMainClass = toggleFeatures({
+            name: 'isAppRedesigned',
+            on: () => cls.triggerRedesigned,
+            off: () => cls.triggerDeprecated,
+        });
+        const iconMainClass = toggleFeatures({
+            name: 'isAppRedesigned',
+            on: () => cls.iconRedesigned,
+            off: () => cls.iconDeprecated,
+        });
+        return (
+            <RadixAccordion.Header className={cls.header}>
+                <RadixAccordion.Trigger
+                    className={classNames(cls.trigger, {}, [triggerMainClass])}
+                    {...props}
+                    ref={forwardedRef}
+                >
+                    {children}
+                    <ToggleFeaturesComponent
+                        feature="isAppRedesigned"
+                        on={
+                            <Icon
+                                aria-hidden
+                                Svg={ArrowDownIcon}
+                                {...ICON_PROPS}
+                                className={classNames(cls.icon, {}, [
+                                    iconMainClass,
+                                ])}
+                            />
+                        }
+                        off={
+                            <IconDeprecated
+                                aria-hidden
+                                Svg={ArrowDownIcon}
+                                {...ICON_PROPS}
+                                className={classNames(cls.icon, {}, [
+                                    iconMainClass,
+                                ])}
+                            />
+                        }
+                    />
+                </RadixAccordion.Trigger>
+            </RadixAccordion.Header>
+        );
+    },
 );
 
 interface AccordionContentProps extends RadixAccordion.AccordionContentProps {
@@ -65,15 +96,22 @@ interface AccordionContentProps extends RadixAccordion.AccordionContentProps {
 }
 
 const AccordionContent = forwardRef<HTMLDivElement, AccordionContentProps>(
-    ({ children, className, ...props }, forwardedRef) => (
-        <RadixAccordion.Content
-            className={cls.content}
-            {...props}
-            ref={forwardedRef}
-        >
-            <div className={cls.contentInner}>{children}</div>
-        </RadixAccordion.Content>
-    ),
+    ({ children, className, ...props }, forwardedRef) => {
+        const contentMainClass = toggleFeatures({
+            name: 'isAppRedesigned',
+            on: () => cls.contentRedesigned,
+            off: () => cls.contentDeprecated,
+        });
+        return (
+            <RadixAccordion.Content
+                className={classNames(cls.content, {}, [contentMainClass])}
+                {...props}
+                ref={forwardedRef}
+            >
+                <div className={cls.contentInner}>{children}</div>
+            </RadixAccordion.Content>
+        );
+    },
 );
 
 export const Accordion = (props: AccordionInterface) => {
@@ -82,10 +120,15 @@ export const Accordion = (props: AccordionInterface) => {
     const renderItems = () =>
         items.map((item, index) => {
             const itemValue = item.value || `item-${index + 1}`;
+            const itemMainClass = toggleFeatures({
+                name: 'isAppRedesigned',
+                on: () => cls.itemRedesigned,
+                off: () => cls.itemDeprecated,
+            });
             return (
                 <RadixAccordion.Item
                     key={itemValue}
-                    className={cls.item}
+                    className={classNames(cls.item, {}, [itemMainClass])}
                     value={itemValue}
                 >
                     <AccordionTrigger>{item.trigger}</AccordionTrigger>
