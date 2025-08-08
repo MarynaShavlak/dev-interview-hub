4624119c388743ee96f9de8a6f6f92129cf2f6bb
Maryna Shavlak <shavlakmaryna@gmail.com>
2025-08-08 15:10:23 +0300

    and mobile styles for Editable profile card

--- src/features/EditableProfileCard/ui/UserCard/UserCardRedesigned/UserCardRedesigned.tsx
+++ src/features/EditableProfileCard/ui/UserCard/UserCardRedesigned/UserCardRedesigned.tsx
@@ -1,5 +1,6 @@
 import { memo } from 'react';
 import { useTranslation } from 'react-i18next';
+import { isMobile } from 'react-device-detect';
 import { classNames } from '@/shared/lib/classes/classNames/classNames';
 import { getFlexClasses } from '@/shared/lib/classes/getFlexClasses/getFlexClasses';
 import { Card } from '@/shared/ui/redesigned/Card';
@@ -39,6 +40,81 @@ export const UserCardRedesigned = memo((props: UserCardProps) => {
             'profile',
         );
 
+    const leftInputs = (
+        <>
+            <Input
+                value={data?.email}
+                label={`${t('Email')}:`}
+                readonly
+                disabled
+                data-testid="UserCard.email"
+            />
+            <Input
+                value={data?.firstname}
+                label={`${t("Ім'я")}:`}
+                onChange={onChangeFirstname}
+                readonly={readonly}
+                disabled={readonly}
+                data-testid="UserCard.firstname"
+                validations={validConfig.firstname}
+                errors={firstnameErrors}
+            />
+            <Input
+                value={data?.lastname}
+                label={`${t('Прізвище')}:`}
+                onChange={onChangeLastname}
+                readonly={readonly}
+                disabled={readonly}
+                data-testid="UserCard.lastname"
+                validations={validConfig.lastname}
+                errors={lastnameErrors}
+            />
+
+            <Input
+                value={data?.age || ''}
+                label={`${t('Вік')}:`}
+                onChange={onChangeAge}
+                readonly={readonly}
+                disabled={readonly}
+                digitsOnly
+                data-testid="UserCard.age"
+            />
+        </>
+    );
+    const rightInputs = (
+        <>
+            <Input
+                value={data?.username}
+                label={`${t("Ім'я користувача")}:`}
+                onChange={onChangeUsername}
+                readonly={readonly}
+                disabled={readonly}
+                data-testid="UserCard.username"
+                validations={validConfig.lastname}
+                errors={usernameErrors}
+            />
+
+            <CurrencySelect
+                value={data?.currency}
+                onChange={onChangeCurrency}
+                readonly={readonly}
+            />
+            <CountrySelect
+                value={data?.country}
+                onChange={onChangeCountry}
+                readonly={readonly}
+            />
+            <Input
+                value={data?.city || ''}
+                label={`${t('Місто')}:`}
+                onChange={onChangeCity}
+                readonly={readonly}
+                disabled={readonly}
+                data-testid="UserCard.city"
+            />
+        </>
+    );
+
     return (
         <Card
             padding="24"
@@ -51,78 +127,22 @@ export const UserCardRedesigned = memo((props: UserCardProps) => {
                 onFileUpload={onFileUpload}
             />
 
-            <HStack gap="24" max align="start">
+            {!isMobile && (
+                <HStack gap="24" max align="start">
+                    <VStack gap="16" max>
+                        {leftInputs}
+                    </VStack>
+                    <VStack gap="16" max>
+                        {rightInputs}
+                    </VStack>
+                </HStack>
+            )}
+            {isMobile && (
                 <VStack gap="16" max>
-                    <Input
-                        value={data?.email}
-                        label={`${t('Email')}:`}
-                        readonly
-                        disabled
-                        data-testid="UserCard.email"
-                    />
-                    <Input
-                        value={data?.firstname}
-                        label={`${t("Ім'я")}:`}
-                        onChange={onChangeFirstname}
-                        readonly={readonly}
-                        disabled={readonly}
-                        data-testid="UserCard.firstname"
-                        validations={validConfig.firstname}
-                        errors={firstnameErrors}
-                    />
-                    <Input
-                        value={data?.lastname}
-                        label={`${t('Прізвище')}:`}
-                        onChange={onChangeLastname}
-                        readonly={readonly}
-                        disabled={readonly}
-                        data-testid="UserCard.lastname"
-                        validations={validConfig.lastname}
-                        errors={lastnameErrors}
-                    />
-
-                    <Input
-                        value={data?.age || ''}
-                        label={`${t('Вік')}:`}
-                        onChange={onChangeAge}
-                        readonly={readonly}
-                        disabled={readonly}
-                        digitsOnly
-                        data-testid="UserCard.age"
-                    />
+                    {leftInputs}
+                    {rightInputs}
                 </VStack>
-                <VStack gap="16" max>
-                    <Input
-                        value={data?.username}
-                        label={`${t("Ім'я користувача")}:`}
-                        onChange={onChangeUsername}
-                        readonly={readonly}
-                        disabled={readonly}
-                        data-testid="UserCard.username"
-                        validations={validConfig.lastname}
-                        errors={usernameErrors}
-                    />
-
-                    <CurrencySelect
-                        value={data?.currency}
-                        onChange={onChangeCurrency}
-                        readonly={readonly}
-                    />
-                    <CountrySelect
-                        value={data?.country}
-                        onChange={onChangeCountry}
-                        readonly={readonly}
-                    />
-                    <Input
-                        value={data?.city || ''}
-                        label={`${t('Місто')}:`}
-                        onChange={onChangeCity}
-                        readonly={readonly}
-                        disabled={readonly}
-                        data-testid="UserCard.city"
-                    />
-                </VStack>
-            </HStack>
+            )}
         </Card>
     );
 });
